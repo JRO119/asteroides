968a821854b65f5ecb6ac04ea5b30e24440fd7ac
JROE119 <j.roesing@email.de>
2025-09-19 21:43:43 +0200

    triangle draw screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 
 def main():
@@ -9,15 +11,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    triangle = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
     while 1 == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        
+        triangle.draw(screen)
 
 
 
         ####end###
+        dt = clock.tick(60) / 1000          
         pygame.display.flip
 
 
