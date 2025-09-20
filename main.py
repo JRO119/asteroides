577dc2d32a42a7c7fdef77dc6135264c7b971198
JROE119 <j.roesing@email.de>
2025-09-20 13:28:29 +0200

    start asteroids class

--- main.py
+++ main.py
@@ -5,28 +5,37 @@ from circleshape import *
 
 
 def main():
-   
+    ### starting tasks ###
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    ### setup variables ###
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    ### initiate player ###
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
-    while 1 == 1:
+    
+    ### start game loop ###
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         pygame.Surface.fill(screen, (0, 0, 0))
         dt = clock.tick(60) / 1000
-        player.update(dt)
-        player.draw(screen)
-
-
-
-        ####end###
-        dt = clock.tick(60) / 1000          
+        updatable.update(dt)
+        for dr in drawable:
+            dr.draw(screen)        
+        
+        ### do last after all drawings ##
         pygame.display.flip()
 
 
